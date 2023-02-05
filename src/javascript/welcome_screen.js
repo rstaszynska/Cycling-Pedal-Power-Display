4b6bc599c3527749941c69ed05c982cd2503d723
Roksana Staszynska <2450370S@student.gla.ac.uk>
2023-02-05 19:49:37 +0000

    Minor styling fixes

--- src/javascript/welcome_screen.js
+++ src/javascript/welcome_screen.js
@@ -3,6 +3,9 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 });
 let display = params.display;
 
+if (display === "large") {
+    document.querySelector("#touchscreen-message").remove();
+}
 
 function next() {
     location.href = "appliance_selection.html?display=" + display;
